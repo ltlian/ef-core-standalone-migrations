e4003c2c0fa818fec25a64a03220172281910e1a
lars <larstomaslian@gmail.com>
2025-11-18 16:06:58 +0100

    Add user migration example

---
+++ src/MusicLibrary.Persistence.SqlServer/MigrationBuilderExtensions.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using Microsoft.EntityFrameworkCore.Migrations.Operations.Builders;
+
+namespace MusicLibrary.Persistence.SqlServer;
+
+public static class MigrationBuilderExtensions
+{
+    private const string MusicOwnerRole = "music_data_owner";
+
+    extension(MigrationBuilder migrationBuilder)
+    {
+        public OperationBuilder<SqlOperation> CreateMusicDataOwnerRole()
+        {
+            migrationBuilder.EnsureSqlServerProvider();
+            return migrationBuilder.Sql($"""
+                IF NOT EXISTS (SELECT 1 FROM sys.database_principals WHERE [type] = 'R' AND name = N'{MusicOwnerRole}')
+                BEGIN
+                    CREATE ROLE [{MusicOwnerRole}];
+                END
+
+                GRANT SELECT, INSERT, UPDATE, DELETE
+                    ON SCHEMA :: [dbo]
+                    TO [{MusicOwnerRole}];
+                """);
+        }
+
+        public OperationBuilder<SqlOperation> DropMusicDataOwnerRole()
+        {
+            migrationBuilder.EnsureSqlServerProvider();
+            return migrationBuilder.Sql($"DROP ROLE IF EXISTS [{MusicOwnerRole}];");
+        }
+
+        public OperationBuilder<SqlOperation> CreateUser(string login, string password)
+        {
+            migrationBuilder.EnsureSqlServerProvider();
+            return migrationBuilder.Sql($"""
+                IF NOT EXISTS (SELECT 1 FROM sys.database_principals WHERE [type] = 'S' AND name = N'{login}')
+                BEGIN
+                    CREATE LOGIN [{login}] WITH PASSWORD = N'{password}';
+                    CREATE USER [{login}] FOR LOGIN [{login}];
+                END
+
+                ALTER ROLE [{MusicOwnerRole}] ADD MEMBER [{login}];
+                """);
+        }
+
+        public OperationBuilder<SqlOperation> DropUser(string login)
+        {
+            migrationBuilder.EnsureSqlServerProvider();
+            return migrationBuilder.Sql($"""
+                IF EXISTS (SELECT 1 FROM sys.database_principals WHERE [type] = 'S' AND name = N'{login}')
+                BEGIN
+                    ALTER ROLE [{MusicOwnerRole}] DROP MEMBER [{login}];
+                    DROP USER [{login}];
+                    DROP LOGIN [{login}];
+                END
+                """);
+        }
+
+        private void EnsureSqlServerProvider()
+        {
+            if (!migrationBuilder.IsSqlServer())
+            {
+                throw new Exception("Unexpected provider.");
+            }
+        }
+    }
+}
---
+++ src/MusicLibrary.Persistence.SqlServer/Migrations/20251118121032_AddAppUserRole.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicLibrary.Persistence.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAppUserRole() : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateMusicDataOwnerRole();
+
+            // During development, the simplest approach is to generate a script using `dotnet ef migrations script`
+            // and replacing the password manually.
+            // For automated deployments or production scenarios, see https://learn.microsoft.com/en-us/sql/relational-databases/security/sql-server-security-best-practices?view=sql-server-ver17#identities-and-authentication
+            migrationBuilder.CreateUser("music-api", "{API_USER_PASSWORD}");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUser("music-api");
+            migrationBuilder.DropMusicDataOwnerRole();
+        }
+    }
+}
